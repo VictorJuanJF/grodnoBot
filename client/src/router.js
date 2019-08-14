7acc0f2f7ff58160f2da6c790c2c1ea155c3c050
Victor Juan <vj.jimenez96@gmail.com>
2019-08-14 11:43:17 -0500

    webview

--- client/src/router.js
+++ client/src/router.js
@@ -4,56 +4,61 @@ import localStorage from "localStorage"
 Vue.use(Router);
 
 let routes = [{
-    path: '/dashboard',
-    component: require('./views/dashboard.vue').default,
-    meta: {
-        requiresAuth: true
+        path: '/dashboard',
+        component: require('./views/dashboard.vue').default,
+        meta: {
+            requiresAuth: true
+        },
+        children: [{
+            path: '',
+            name: 'dashboard',
+            redirect: {
+                name: 'intent'
+            }
+        }, {
+            path: 'intenciones',
+            name: 'intent',
+            component: require('./views/drawerPages/intent.vue').default
+        }, {
+            path: 'intenciones/:id',
+            name: 'updateIntent',
+            component: require('./views/drawerPages/updateIntent.vue').default
+        }, {
+            path: 'menu-persistente',
+            name: 'persistantMenu',
+            component: require('./views/drawerPages/persistantMenu.vue').default
+        }, {
+            path: 'agencias',
+            name: 'agencies',
+            component: require('./views/drawerPages/agencies.vue').default
+        }]
+    }, {
+        path: '/',
+        component: require('./views/auth/home.vue').default,
+        meta: {
+            guest: true
+        },
+        children: [{
+            path: '',
+            redirect: '/login'
+        }, {
+            path: '/login',
+            name: 'login',
+            component: require('./views/auth/login.vue').default
+        }, {
+            path: 'registro',
+            name: 'register',
+            component: require('./views/auth/register.vue').default
+        }]
+    }, {
+        path: '/webview',
+        component: require('./views/webview/servicios.vue').default
     },
-    children: [{
-        path: '',
-        name: 'dashboard',
-        redirect: {
-            name: 'intent'
-        }
-    }, {
-        path: 'intenciones',
-        name: 'intent',
-        component: require('./views/drawerPages/intent.vue').default
-    }, {
-        path: 'intenciones/:id',
-        name: 'updateIntent',
-        component: require('./views/drawerPages/updateIntent.vue').default
-    }, {
-        path: 'menu-persistente',
-        name: 'persistantMenu',
-        component: require('./views/drawerPages/persistantMenu.vue').default
-    }, {
-        path: 'agencias',
-        name: 'agencies',
-        component: require('./views/drawerPages/agencies.vue').default
-    }]
-}, {
-    path: '/',
-    component: require('./views/auth/home.vue').default,
-    meta: {
-        guest: true
-    },
-    children: [{
-        path: '',
-        redirect: '/login'
-    }, {
-        path: '/login',
-        name: 'login',
-        component: require('./views/auth/login.vue').default
-    }, {
-        path: 'registro',
-        name: 'register',
-        component: require('./views/auth/register.vue').default
-    }]
-}, {
-    path: '*',
-    component: require('./views/errorPage/errorPage.vue').default
-}];
+    {
+        path: '*',
+        component: require('./views/errorPage/errorPage.vue').default
+    }
+];
 
 
 const router = new Router({
